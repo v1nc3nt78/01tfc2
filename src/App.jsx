2c7a6a003ca9bac025210c64c25d272fb269cd24
v1nc3nt78 <vicente1978s@gmail.com>
2024-09-02 16:40:51 -0300

    working in app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App (){
+const format = (userName) => `@${userName}`
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return(
+        <section className='App'>
+            <TwitterFollowCard 
+                formatUserName={format} 
+                isFollowing 
+                userName="midudev" 
+                name="Miguel Angel Durán"
+            />
+
+            <TwitterFollowCard 
+                formatUserName={format} 
+                isFollowing 
+                userName="pheralb" 
+                name="Pablo Hernandez"
+            />
+           
+            <TwitterFollowCard 
+                formatUserName={format} 
+                isFollowing={false} 
+                userName="vxnder" 
+                name="Vanderhart"
+            />
+        </section>
+    )
 }
-
-export default App
---
+++ src/TwitterFollowCard.jsx
@@ -0,0 +1,24 @@
+export function TwitterFollowCard ({formatUserName, userName, name, isFollowing}){
+    return(
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img 
+                    className='tw-followCard-avatar'
+                    alt="El avatar de midudev" 
+                    src={`https://unavatar.io/${userName}`}/>
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-infoUserName'>
+                        {formatUserName(userName)}
+                    </span>
+                </div>
+            </header>
+
+            <aside>
+                <button className='tw-followCard-button'>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
